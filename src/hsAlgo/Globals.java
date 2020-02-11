b7e6ec2f7b2c0abd08d465ed57ef3f49e8ee6a78
Tanu Rampal <tanurampal@cometnet-10-21-37-82.utdallas.edu>
2020-02-11 14:54:27 -0600

    Edits

--- src/hsAlgo/Globals.java
+++ src/hsAlgo/Globals.java
@@ -3,8 +3,8 @@ package hsAlgo;
 import java.util.concurrent.locks.*;
 
 public class Globals {
-    public static Lock roundBegins = new RentrantLock();
-    public static Condition roundStarts = roundBegin.newCondition();
+    public static Lock roundBegins = new ReentrantLock();
+    public static Condition roundStarts = roundBegins.newCondition();
     public static int roundNumber = 0;
     public Globals() {
     }
