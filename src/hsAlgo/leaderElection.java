59facad87e01c856eebe9142dab774da8a1d2acf
Tanu Rampal <tanurampal@Tanus-MacBook-Pro.local>
2020-02-10 22:52:35 -0600

    Initialise array of processes

--- src/hsAlgo/leaderElection.java
+++ src/hsAlgo/leaderElection.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 public class leaderElection{
     public void Run(int[] processIds) {
         //master spawns new threads
+        ArrayList<Process> threads = new ArrayList<Process>();
         int n = processIds.length;
         Lock roundBegin = new RentrantLock(); // roundBegin is the lock and roundStarts in the condition that all processes wait on
         Condition roundStarts = roundBegin.newCondition();
@@ -18,6 +19,8 @@ public class leaderElection{
         MessageChannel first_receive = new MessageChannel();
         MessageChannel send_to_pred = first_send;
         MessageChannel recv_from_pred = first_receive;
+
+
         for (int i = 0; i < n; i++) {
             MessageChannel send_to_succ;
             MessageChannel recv_from_succ;
