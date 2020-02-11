fac97de834d1cf053d7c5beb2ce61be430192f01
virajmavani <viraj.mavani1996@gmail.com>
2020-02-11 11:51:56 -0600

    Minor changes - 2

--- src/hsAlgo/Main.java
+++ src/hsAlgo/Main.java
@@ -23,6 +23,8 @@ public class Main{
             ids.add(id);
         }
 
+        br.close();
+
         master.Run( ids );
     }
 }
--- src/hsAlgo/MessageChannel.java
+++ src/hsAlgo/MessageChannel.java
@@ -5,6 +5,8 @@ import java.util.concurrent.locks.*;
 public class MessageChannel {
     private final Lock msgLock = new ReentrantLock();
     private final Condition msgSent = msgLock.newCondition();
+    private final Condition msgRead = msgLock.newCondition();
+    Message message;
 
     public void SendMessage(Message msg) {
         //acquire a lock to write
@@ -12,7 +14,9 @@ public class MessageChannel {
 
         //writing the message to queue and signal the waiting thread to read the message
         try {
-
+            msgRead.wait();
+            this.message = msg;
+            msgSent.signalAll();
         } catch (Exception e) {
             System.out.println("Unable to send message. Exited with exception: "+e);
         } finally {
@@ -21,7 +25,23 @@ public class MessageChannel {
         }
     }
 
-    public void ReceiveMessage(Message msg) {
+    public Message ReceiveMessage() {
+        // acquire lock to read the message content
+        msgLock.lock();
+        Message msg = new Message(0, (byte) 0, 0);
 
+        // reading the message and signal that waiting thread can write 
+        try {
+            msgSent.wait();
+            msg = this.message;
+            this.message = null;
+            msgRead.signalAll();
+        } catch (Exception e) {
+            System.out.println("Unable to read message. Exited with exception: "+e);
+        } finally {
+            msgLock.unlock();
+        }
+
+        return msg;
     }
 }
